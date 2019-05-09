08e71e4514ab11ffdea4c4af22ccbe495ff8cf6b
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-09 21:56:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,10 @@
 def my_hash
 your_hash = {"name" => "Steve", "age" => 31}
-  
 end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases "}
-  
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -31,6 +24,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+      
+    shipping_manifest =           
   # add 2 muskets to the shipping_manifest hash below
 
 
