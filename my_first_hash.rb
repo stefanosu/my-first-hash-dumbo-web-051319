b1155e9778abaf03f1e845b562084da6092dea2b
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-09 21:58:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,8 +26,8 @@ def adding
                  }
       
     shipping_manifest["muskets"] = 2 
-    
-  # add 2 muskets to the shipping_manifest hash below
+    shipping_manifest["gun powder"] = 4 
+    shipping_manifest[""]
 
 
 
